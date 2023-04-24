e9bad49bbe211928307e6dc81c3686a0448ac66c
Djafny <jafnyabdad@gmail.com>
2023-04-24 13:39:33 +0200

    updated

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ function createStars() {
   }
   
   createStars();
+
+  const body = document.querySelector('body');
+
+let isScrolling;
+
+// Add the 'hide-thumb' class when scrolling starts
+body.addEventListener('scroll', () => {
+  clearTimeout(isScrolling);
+  body.classList.add('hide-thumb');
+
+  isScrolling = setTimeout(() => {
+    // Remove the 'hide-thumb' class when scrolling stops
+    body.classList.remove('hide-thumb');
+  }, 100);
+});
+
+
+
   
